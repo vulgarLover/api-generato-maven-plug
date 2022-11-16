e1b7ff39cb8ba69a6adc4b08b623aede688455a3
碧莲 <wb-shenwei@cai-inc.com>
2022-11-16 18:38:00 +0800

    兼容yapi接口上传

--- src/main/java/com/apigcc/maven/ApigccMojo.java
+++ src/main/java/com/apigcc/maven/ApigccMojo.java
@@ -1,7 +1,8 @@
 package com.apigcc.maven;
 
-import com.apigcc.core.Apigcc;
-import com.apigcc.core.Context;
+import com.alibaba.fastjson.JSON;
+import com.ysar.core.Apigcc;
+import com.ysar.core.Context;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
@@ -33,10 +34,15 @@ public class ApigccMojo extends AbstractMojo {
     String dependency;
     @Parameter
     String jar;
+    // 要上传的服务类型，暂时只支持 yapi
     @Parameter
-    String version;
+    String targetServer;
+    // 上传目标服务的接口地址
     @Parameter
-    String css;
+    String serverUrl;
+    // 针对于yapi中的项目的token
+    @Parameter
+    String projectToken;
 
     @Override
     public void execute() {
@@ -68,7 +74,7 @@ public class ApigccMojo extends AbstractMojo {
                 context.addJar(abs(dir));
             }
         }
-        context.setId(id != null ? id : project.getName());
+//        context.setId(id != null ? id : project.getName());
         if (build != null) {
             context.setBuildPath(abs(build));
         } else {
@@ -84,19 +90,23 @@ public class ApigccMojo extends AbstractMojo {
         } else if (project.getDescription() != null) {
             context.setDescription(project.getDescription());
         }
-        if (version != null) {
-            context.setVersion(version);
-        } else if (project.getVersion() != null) {
-            context.setVersion(project.getVersion());
+        if (targetServer == null) {
+            throw new RuntimeException("请配置目标应用: targetServer");
         }
-        if (css != null) {
-            context.setCss(css);
+        if (serverUrl == null) {
+            throw new RuntimeException("目标服务url不能为空 serverUrl");
         }
+        if (serverUrl.endsWith("/")) {
+            serverUrl = serverUrl.substring(0, serverUrl.length() - 1);
+        }
+        context.setServerUrl(serverUrl);
+        context.setTargetServer(targetServer);
+        context.setProjectToken(projectToken);
 
+        System.out.println(JSON.toJSONString(context));
         Apigcc apigcc = new Apigcc(context);
         apigcc.parse();
-        apigcc.render();
-
+        apigcc.upload();
     }
 
     private MavenProject findParent(MavenProject mp) {
@@ -179,19 +189,27 @@ public class ApigccMojo extends AbstractMojo {
         this.jar = jar;
     }
 
-    public String getVersion() {
-        return version;
+    public String getTargetServer() {
+        return targetServer;
     }
 
-    public void setVersion(String version) {
-        this.version = version;
+    public void setTargetServer(String targetServer) {
+        this.targetServer = targetServer;
     }
 
-    public String getCss() {
-        return css;
+    public String getServerUrl() {
+        return serverUrl;
     }
 
-    public void setCss(String css) {
-        this.css = css;
+    public void setServerUrl(String serverUrl) {
+        this.serverUrl = serverUrl;
+    }
+
+    public String getProjectToken() {
+        return projectToken;
+    }
+
+    public void setProjectToken(String projectToken) {
+        this.projectToken = projectToken;
     }
 }
